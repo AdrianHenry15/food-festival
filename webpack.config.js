5b9df6b124b534e0e5fc48fc4a9a02b3d5b8ac59
Adrian <adrianhenry2115@gmail.com>
2022-04-23 12:25:22 -0400

    assets js folder debugged and refactored

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,18 @@
-const path = require("path");
-const webpack = require("webpack");
-const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
-const WebpackPwaManifest = require("webpack-pwa-manifest");
-
+const webpack = require('webpack');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
+const path = require('path');
 
 const config = {
     entry: {
-        app: "./assets/js/script.js",
-        events: "./assets/js/events.js",
-        schedule: "./assets/js/schedule.js",
-        tickets: "./assets/js/tickets.js"
+        app: './assets/js/script.js',
+        events: './assets/js/events.js',
+        schedule: './assets/js/schedule.js',
+        tickets: './assets/js/tickets.js'
     },
     output: {
         filename: '[name].bundle.js',
         path: `${__dirname}/dist`
-
     },
     module: {
         rules: [
@@ -26,11 +24,10 @@ const config = {
                         options: {
                             esModule: false,
                             name(file) {
-                                return "[path][name].[ext]"
-
+                                return '[path][name].[ext]';
                             },
-                            publicPath: function (url) {
-                                return url.replace("../", "/assets/")
+                            publicPath(url) {
+                                return url.replace('../', '/assets/');
                             }
                         }
                     },
@@ -43,29 +40,31 @@ const config = {
     },
     plugins: [
         new webpack.ProvidePlugin({
-            $: "jquery",
-            jquery: "jquery"
+            $: 'jquery',
+            jQuery: 'jquery'
         }),
         new BundleAnalyzerPlugin({
-            analyzerMode: "static", // the report outputs to an HTML file in the dist folder
+            analyzerMode: 'static'
         }),
         new WebpackPwaManifest({
-            name: "Food Event",
-            short_name: "Foodies",
-            description: "An app that allows you to view upcoming food events.",
-            start_url: "../index.html",
-            background_color: "#01579b",
-            theme_color: "#ffffff",
+            name: 'Food Event',
+            short_name: 'Foodies',
+            description: 'An app that allows you to view upcoming food events.',
+            start_url: '../index.html',
+            background_color: '#01579b',
+            theme_color: '#ffffff',
             fingerprints: false,
             inject: false,
-            icons: [{
-                src: path.resolve("assets/img/icons/icon-512x512.png"),
-                sizes: [96, 128, 192, 256, 384, 512],
-                destination: path.join("assets", "icons")
-            }]
+            icons: [
+                {
+                    src: path.resolve('assets/img/icons/icon-512x512.png'),
+                    sizes: [96, 128, 192, 256, 384, 512],
+                    destination: path.join('assets', 'icons')
+                }
+            ]
         })
     ],
     mode: 'development'
-}
+};
 
 module.exports = config;
